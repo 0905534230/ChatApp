d8b1246ef25de6be5122d801fc2cdaa8b0587334
0905534230 <quan.hoang170203@vnuk.edu.vn>
2019-06-06 15:39:55 +0700

    .

--- app/src/main/java/com/example/chatapp/MessageActivity.java
+++ app/src/main/java/com/example/chatapp/MessageActivity.java
@@ -365,7 +365,7 @@ public class MessageActivity extends AppCompatActivity {
         reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
 
         HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("Status", status);
+        hashMap.put("status", status);
 
         reference.updateChildren(hashMap);
 
